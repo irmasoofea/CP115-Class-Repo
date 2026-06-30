3dbaf37cc58ced91b350d6e678798031dd964bdb
Joandra2924 <putrij0916@gmail.com>
2026-06-30 15:38:36 +0800

    update

---
+++ labs/lab03/exercise3/lab-03-3.py
@@ -0,0 +1,10 @@
+print("Enter size in KB")
+kilobyte = float(input())
+megabyte = kilobyte / 1024
+gigabyte = megabyte / 1024
+terabyte = gigabyte / 1024
+petabyte = terabyte / 1024
+print("Megabyte: ", megabyte)
+print("Gigabyte: ", gigabyte)
+print("Terabyte: ", terabyte)
+print("Petabyte: ", petabyte)
