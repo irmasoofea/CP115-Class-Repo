61051ddef7900b37b6e46832ad74fd7a9a891bac
Joandra2924 <putrij0916@gmail.com>
2026-08-03 09:09:10 +0800

    up

---
+++ labs/lab05/exercise3.py
@@ -0,0 +1,6 @@
+import random
+random_number = random.randint(1, 100)
+from random import choice, shuffle
+class_name = input("Enter your class name: ")
+print("Class name:", class_name)
+print("Random number between 1 and 100:", random_number)
