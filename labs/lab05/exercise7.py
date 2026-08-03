17cfe9d48f97e0e03af3ff6d68a2a235b964ccbe
Joandra2924 <putrij0916@gmail.com>
2026-08-03 09:29:43 +0800

    up

---
+++ labs/lab05/exercise7.py
@@ -0,0 +1,10 @@
+import math
+from math import sqrt, pow, sin, cos
+square_root = int(sqrt(25))
+power_result = int(pow(2, 3))
+number = int(input("Enter a number: "))
+sine_number = int(sin(number))
+
+print("Square root of number:", number, "is", square_root)
+print("Power result:", power_result)
+print("Sine of number:", number, "is", sine_number)
