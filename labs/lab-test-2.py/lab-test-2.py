b2a8b15b5e6819dcf972ba6c37ad8a1c0b363c18
Joandra2924 <putrij0916@gmail.com>
2026-08-17 08:14:56 +0800

    up

--- labs/lab-test-2.py/lab-test-2.py
+++ labs/lab-test-2.py/lab-test-2.py
@@ -6,5 +6,5 @@ first_mark= 2
 second_mark= 10
 total_mark= first_mark*second_mark
 
-assignment_info= f"Name:{Name}\t\t\tMatric.No:{Matric_No}\n\n*\t\t\t*\n**\t\t**\n***\t***\n********\n***\t***\n**\t\t**\n*\t\t\t*\n\nThis is my\n\tsecond\n\t\tassignment\nI want 2x10 marks, which is {total_mark} full marks"
+assignment_info= f"Name:{Name}\t\t\tMatric.No:{Matric_No}\n\n*\t\t\t*\n**\t\t**\n***\t***\n********\n***\t***\n**\t\t**\n*\t\t\t*\n\nThis is my\n\tsecond\n\t\tpython -m venv cp115_envassignment\nI want 2x10 marks, which is {total_mark} full marks"
 print(assignment_info)
